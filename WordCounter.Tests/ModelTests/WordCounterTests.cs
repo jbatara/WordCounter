a584cfc70a889fed3efaa2b2f12549213a81763e
jbatara <jennifer.batara.dev@gmail.com>
2019-10-04 15:22:13 -0700

    CreateList Pass test

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -11,12 +11,12 @@ namespace WordCounter.Testing
         [TestMethod]
         public void CreateList_CheckIfOutputIsListofWords_ListofWords()
         {
-            string userInput = "This is an examples of a valid input. \n It even takes things like extra character$.";
+            string userInput = "This is an example of a valid input. \n It even takes things like extra character$.";
 
-            List<string> expected =  new List<string>() {"This", "is", "an","example","of","a","valid","input.", "It", "even", "takes", "things", "like", "extra", "character$."};
-            List<string> actual = RepeatCounter.CreateList(userInput);
+            string[] expected =  new string[] {"This", "is", "an","example","of","a","valid","input.", "It", "even", "takes", "things", "like", "extra", "character$."};
+            string[] actual = RepeatCounter.CreateList(userInput);
 
-            Assert.AreEqual(expected,actual);
+            CollectionAssert.AreEqual(expected,actual);
         }
 
         [TestMethod]
