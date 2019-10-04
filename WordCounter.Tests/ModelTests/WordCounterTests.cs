a09da458edc77c7960ef8f6236232193bf323729
jbatara <jennifer.batara.dev@gmail.com>
2019-10-04 15:02:55 -0700

    Add tests for remaining methods. Fix 'Dictionary' typo.

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -29,5 +29,39 @@ namespace WordCounter.Testing
 
             Assert.AreEqual(expected,actual);
         }
+
+        [TestMethod]
+        public void UniqueValues_CheckIfOutputIsAllUniqueWords_UniqueStringList()
+        {
+            List<string> cleanedWords = new List<string>() {"this", "is", "an","example","of","a","valid","input", "it", "even", "will", "take", "a", "few","things", "like", "extra", "character$"};
+
+            List<string> expected =  new List<string>() {"this", "is", "an","example","of","a","valid","input", "it", "even","will", "take","few", "things", "like", "extra", "character$"};
+            List<string> actual = RepeatCounter.UniqueValues(cleanedWords);
+
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void CountOccurances_CheckIfOutputIsDictionary_DictionaryStringInt()
+        {
+            List<string> cleanedWords = new List<string>() {"this", "is", "an","example","of","a","valid","input", "it", "even", "will", "take", "a", "few","things", "like", "extra", "character$"};
+            List<string> uniqueWords = new List<string>() {"this", "is", "an","example","of","a","valid","input", "it", "even","will", "take","few", "things", "like", "extra", "character$"};
+
+            Dictionary<string,int> expected =  new Dictionary<string,int>(){{"this", 1},{"is",1},{"an", 1},{"example",1},{"of",1},{"a",1},{"valid",1},{"input",1},{"it",1},{"even",1},{"takes",1},{"things",1},{"like", 1},{"extra",1},{"character$",1}};
+            Dictionary<string,int> actual = RepeatCounter.CountOccurrences(cleanedWords, uniqueWords);
+
+            Assert.AreEqual(expected,actual);
+        }
+
+          [TestMethod]
+        public void WordCount_CheckIfOutputIsString_String()
+        {
+           string userInput = "This is an examples of a valid input. \n It even takes things like extra character$.";
+
+            Dictionary<string,int> expected =  new Dictionary<string,int>(){{"this", 1},{"is",1},{"an", 1},{"example",1},{"of",1},{"a",1},{"valid",1},{"input",1},{"it",1},{"even",1},{"takes",1},{"things",1},{"like", 1},{"extra",1},{"character$",1}};
+            Dictionary<string,int> actual = RepeatCounter.WordCount(userInput);
+
+            Assert.AreEqual(expected,actual);
+        }
     }
 }
