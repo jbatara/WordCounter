00c8ae6d91a734d421eb1cda3df3450a0aeba23e
jbatara <jennifer.batara.dev@gmail.com>
2019-10-04 16:20:05 -0700

    Add logic to overall WordCount method. Pass test

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -67,10 +67,22 @@ namespace WordCounter.Testing
           [TestMethod]
         public void WordCount_CheckIfOutputIsString_String()
         {
-           string userInput = "This is an examples of a valid input. \n It even takes things like extra character$.";
+           string userInput = "This is an example of a valid input. \n It even takes things like extra character$ if you want it to of course.";
+            List<string> cleanedWords = RepeatCounter.CleanList(RepeatCounter.CreateList(userInput));
+            List<string> uniqueWords = RepeatCounter.UniqueValues(cleanedWords);
 
-            Dictionary<string,int> expected =  new Dictionary<string,int>(){{"this", 1},{"is",1},{"an", 1},{"example",1},{"of",1},{"a",1},{"valid",1},{"input",1},{"it",1},{"even",1},{"takes",1},{"things",1},{"like", 1},{"extra",1},{"character$",1}};
-            Dictionary<string,int> actual = RepeatCounter.WordCount(userInput);
+            Dictionary<string,int> expectedDict =  new Dictionary<string,int>(){{"this", 1},{"is",1},{"an", 1},{"example",1},{"of",2},{"a",1},{"valid",1},{"input",1},{"it",2},{"even",1},{"takes",1},{"things",1},{"like", 1},{"extra",1},{"character$",1},{"if",1},{"you",1},{"want",1},{"to",1},{"course",1}};
+            List<int> expected = new List<int>();
+            foreach(string key in expectedDict.Keys)
+            {
+                expected.Add(expectedDict[key]);
+            }
+            Dictionary<string,int> actualDict = RepeatCounter.WordCount(userInput);
+            List<int> actual = new List<int>();
+            foreach(string key in actualDict.Keys)
+            {
+                actual.Add(actualDict[key]);
+            }
 
             CollectionAssert.AreEqual(expected,actual);
         }
